ccd8d3141006a432042941415f9e7b294230d0ae
ohitstom <ohitstom@users.noreply.github.com>
2023-01-12 11:45:33 +0000

    packages

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ async def login():
     await utils.powershell('Connect-MgGraph -Scopes "User.Read", "User.Read.All", "Directory.Read.All", "Group.Read.All"', wait=True, verbose=False)
     access_proc = await utils.powershell("Get-AzAccessToken -ResourceUrl 'https://graph.microsoft.com/' | ConvertTo-Json", wait=True, verbose=False)
     access = (await access_proc.stdout.read()).decode("utf-8")
+
     package = {
         'Token': json.loads(access)['Token'],
         'Expiry': int(''.join(filter(str.isdigit, json.loads(access)['ExpiresOn']))),
@@ -57,22 +58,39 @@ async def package_check():
     azuread_proc = await utils.powershell("Get-Module -ListAvailable -Name AzureAD", wait=True, verbose=False)
     azuread = (await azuread_proc.stdout.read()).decode("utf-8")
     
-    if azuread == "[]":
+    azread_proc = await utils.powershell("Get-Module -ListAvailable -Name Az.Accounts", wait=True, verbose=False)
+    azread = (await azread_proc.stdout.read()).decode("utf-8")
+
+    if azuread != "" and azread != "":
+        print("AzureAD and Az.Accounts powershell modules installed")
+        await login()
+
+    if azuread == "":
         print("AzureAD powershell module not installed")
         print("Installing AzureAD powershell module")
+        await utils.powershell("Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force", wait=True, verbose=False)
         install_proc = await utils.powershell("Install-Module -Name AzureAD -Scope CurrentUser -Force", wait=True, verbose=False)
         install = (await install_proc.stdout.read()).decode("utf-8")
         if install == "":
             print("AzureAD powershell module installed")
-            await login()
         else:
             print("Error installing AzureAD powershell module")
             print("Please install AzureAD powershell module manually")
             print("Exiting...")
             sys.exit()
-    else:
-        print("AzureAD powershell module found")
-        await login()
+    
+    if azread == "":
+        print("Az.Accounts powershell module not installed")
+        print("Installing Az.Accounts powershell module (This can take a long time)")
+        install_proc = await utils.powershell("Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force", wait=True, verbose=False)
+        install = (await install_proc.read()).decode("utf-8")
+        if install == "":
+            print("Az.Accounts powershell module installed")
+        else:
+            print("Error installing Az.Accounts powershell module")
+            print("Please install Az.Accounts powershell module manually")
+            print("Exiting...")
+            sys.exit()
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
